791f0270140fd3de73643c1fd611bba043f1f2de
Youngwoo <zestlee1106@gmail.com>
2023-02-06 12:56:43 +0900

    styled component 적용

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
+import styled from "styled-components";
+
+const Father = styled.div`
+  display: flex;
+`;
+
+const BoxOne = styled.div`
+  background-color: teal;
+  width: 100px;
+  height: 100px;
+`;
+
+const BoxTwo = styled.div`
+  background-color: tomato;
+  width: 100px;
+  height: 100px;
+`;
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Father>
+      <BoxOne />
+      <BoxTwo />
+    </Father>
   );
 }
 
