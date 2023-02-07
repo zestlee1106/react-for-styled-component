ca929181c26590cfec64de822fdab17ea1928d06
Youngwoo <zestlee1106@gmail.com>
2023-02-07 12:31:18 +0900

    as 를 사용하여 html 태그 변경해주기
    - styled Component 에 href 등 html 속성을 바로 사용해줄수 있다

--- src/App.js
+++ src/App.js
@@ -4,22 +4,20 @@ const Father = styled.div`
   display: flex;
 `;
 
-const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
-`;
-
-const Circle = styled(Box)`
-  /* 스타일을 extend (확장) 하기 */
-  border-radius: 50px;
+const Btn = styled.button`
+  color: white;
+  background-color: tomato;
+  border: 0;
+  border-radius: 15px;
 `;
 
 function App() {
   return (
-    <Father>
-      <Box bgColor="teal" />
-      <Circle bgColor="tomato" />
+    <Father as="header">
+      <Btn>Login</Btn>
+      <Btn as="a" href="/">
+        Login
+      </Btn>
     </Father>
   );
 }
