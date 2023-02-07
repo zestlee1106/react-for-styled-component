b9fad961eea68e73cd2ee9c4dce366df3d7983a5
Youngwoo <zestlee1106@gmail.com>
2023-02-07 12:26:09 +0900

    스타일 확장

--- README.md
+++ README.md
@@ -6,3 +6,8 @@
 - `styled-components` 를 import 한 후, styled.태그명`` 이라고 한 뒤 백틱 안에다 원하는 스타일을 넣으면  
   설정한 컴포넌트명으로 jsx 내에서 컴포넌트를 사용할 수 있다
 - 이것은 중복되는 컴포넌트를 줄일 때 효과적으로 쓰일 수 있다
+
+## 스타일을 확장하기
+
+- 중복되는 스타일들을 줄이기 위해서, Style Component 를 extend 할 수도 있다
+- `styled.div` 대신 `styled(기존컴포넌트명)` 을 하면, Style Component 를 extend 하여 코드 중복을 줄일 수 있다
--- src/App.js
+++ src/App.js
@@ -4,29 +4,22 @@ const Father = styled.div`
   display: flex;
 `;
 
-const BoxOne = styled.div`
-  background-color: teal;
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
   width: 100px;
   height: 100px;
 `;
 
-const BoxTwo = styled.div`
-  background-color: tomato;
-  width: 100px;
-  height: 100px;
-`;
-
-const Text = styled.span`
-  color: white;
+const Circle = styled(Box)`
+  /* 스타일을 extend (확장) 하기 */
+  border-radius: 50px;
 `;
 
 function App() {
   return (
     <Father>
-      <BoxOne>
-        <Text>Hello</Text>
-      </BoxOne>
-      <BoxTwo />
+      <Box bgColor="teal" />
+      <Circle bgColor="tomato" />
     </Father>
   );
 }
